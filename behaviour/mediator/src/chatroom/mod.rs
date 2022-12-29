299288c7d91a0fde0c8c0ba358d2be4eaad36dad
philipdaquin <philipdaquin20@gmail.com>
2022-12-29 20:13:53 +0800

    dfa

---
+++ behaviour/mediator/src/chatroom/mod.rs
@@ -0,0 +1,70 @@
+use std::collections::HashMap;
+
+trait Mediator {
+    fn register(&mut self, colleague: Box<dyn Colleague>); 
+    fn send(&self, sender: &str, message: &str, recipient: Option<&str>);
+}
+
+trait Colleague { 
+    fn new(name: &str, mediator: Box<dyn Mediator>) -> Self where Self: Sized;
+    fn get_name(&self) -> &str;
+    fn send(&self, message: &str, recipient: Option<&str>);
+    fn receive(&self, sender: &str, message: &str);
+}
+
+struct ChatRoom { 
+    users: HashMap<String, Box<dyn Colleague>>
+}
+
+
+impl Mediator for ChatRoom {
+    fn register(&mut self, colleague: Box<dyn Colleague>) {
+        self.users.insert(colleague.get_name().to_string(), colleague);
+    }
+
+    fn send(&self, sender: &str, message: &str, recipient: Option<&str>) {
+        
+        if let Some(recipient) = recipient { 
+            self.users[recipient].receive(sender, message)
+        } else { 
+            for (_, user) in &self.users { 
+                if user.get_name() != sender { 
+                    user.receive(sender, message)
+                }
+            }
+        }
+    }
+}
+
+struct User { 
+    name: String, 
+    mediator: Box<dyn Mediator>
+}
+
+impl Colleague for User {
+    fn new(name: &str, mut mediator: Box<dyn Mediator>) -> Self where Self: Sized {
+
+
+        mediator.register(Box::new(User { 
+            name: name.to_string(),
+            mediator: *Box::to_owned(mediator)
+        }));
+
+        User { 
+            name: name.to_string(),
+            mediator
+        }
+    }
+
+    fn get_name(&self) -> &str {
+        todo!()
+    }
+
+    fn send(&self, message: &str, recipient: Option<&str>) {
+        todo!()
+    }
+
+    fn receive(&self, sender: &str, message: &str) {
+        println!("{} sent a message to {}: {}", sender, self.name, message);
+    }
+}
--- behaviour/mediator/src/main.rs
+++ behaviour/mediator/src/main.rs
@@ -1,3 +1,18 @@
+/**
+ * Mediator design pattern is a behaviourial pattern 
+ *  that aims to promotes loose coupling between objects 
+ *  by introducing a mediator object that coordinates communication 
+ *  between the objects
+ *  
+ *  An example of the Mediator pattern in action might be a chat room 
+ *  application, where each use is an object and the chat room itself is the mediator.
+ * The users can send messages to each other through the mediator (the chat room), which 
+ *  helps decouple the objects from each other and promotes 
+ *  a more organised and maintainable communication structureu 
+ * 
+ */
+
+ mod chatroom;
 fn main() {
     println!("Hello, world!");
 }

8dc1367c2fb270ee109be2d804841b0d488eda82
philipdaquin <philipdaquin20@gmail.com>
2023-01-10 16:42:19 +0800

    template_method

---
+++ behaviour/template_method/src/main.rs
@@ -0,0 +1,17 @@
+
+/**
+ * Template Method is behaviourial design pattern that defines the skeleton of an 
+ * algorithm in the superclass but lets subclasses override specific steps of the algorithm 
+ * without changing its structure 
+ * 
+ * 
+ * The idea behind the pattern is to defne a common structure for 
+ * similar algorithms, and then allow subclasses to provide specific implmentation 
+ * details for certain steps of the algorithm 
+ * 
+ * This way, the overall structure of the algorithm remains the same across all 
+ * subclasses, while the details of certain steps can be modified as needed 
+ */
+fn main() {
+    println!("Hello, world!");
+}

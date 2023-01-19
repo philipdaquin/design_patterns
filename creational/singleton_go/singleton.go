0eb34e71b796d16ab068071087b5b7e828d7d113
philipdaquin <philipdaquin20@gmail.com>
2023-01-19 15:07:41 +0800

    singleton in go

---
+++ creational/singleton_go/singleton.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Singleton struct { 
+	data string 
+}
+
+// mutext is used to ensure that only one goroutine can exevute the code 
+var lock = &sync.Mutex{}
+var instance *Singleton
+
+
+// If the lock was not released in the defer statement, 
+// and the function returns before the lock is released,
+// other goroutines that are waiting for the lock will be blocked 
+// indefinitely and the whole system will be in a deadlock state.
+func getInstance() *Singleton { 
+	if instance == nil { 
+		// Acquire the lock 
+		lock.Lock()
+
+		// Release the lock even if the function returns a nil
+		// `defer` allows you to schedule a function call to be executed 
+		// after the surrounding function return s
+		defer lock.Unlock()
+
+		if instance == nil  {
+			fmt.Println("Creating instance now")
+
+			instance = &Singleton{}
+		}
+	}
+	fmt.Println("Current instance")
+
+	return instance
+}

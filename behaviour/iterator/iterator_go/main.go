467a034da64a5c2f5e676ae825d94d6273bc88fc
philipdaquin <philipdaquin20@gmail.com>
2023-01-20 17:39:08 +0800

    iterator

--- behaviour/iterator/iterator_go/main.go
+++ behaviour/iterator/iterator_go/main.go
@@ -1,2 +1,45 @@
 package main
 
+type Iterator interface { 
+	Next() interface{}
+	HasNext() bool
+}
+
+type ConcreteIterator struct { 
+	items []interface{}
+	index int
+}
+
+func (i *ConcreteIterator) HasNext() bool { 
+	if i.index < len(i.items) { 
+		return true
+	}
+	return false
+}
+func (i *ConcreteIterator) Next() interface{} { 
+	if i.HasNext() { 
+		item := i.items[i.index]
+		i.index +=1
+		return item
+		
+	}
+	return nil
+}
+type Aggregate interface { 
+	Iterator() Iterator
+}
+
+type ConcreteAggregate struct {
+	items []interface{}
+}
+
+func (i *ConcreteAggregate) Iterator() Iterator { 
+	return &ConcreteIterator {i.items, 0 }
+}
+
+
+
+
+func main() { 
+
+}

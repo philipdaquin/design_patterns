24fd50538556bd9f4972ed56234a63aa137f82d9
philipdaquin <philipdaquin20@gmail.com>
2023-02-08 20:29:20 +0800

    state go

---
+++ behaviour/state_go/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+/*
+	State design pattern is a behaviorial design pattern that allows
+	an object to alter its behaviour when its internal state changes
+	- The pattern consists of creating a state abstract class or interface and concrete  state classes
+	for each possible state of the object 
+*/
+
+type Context struct { 
+	current State
+}
+func (self Context) Transitition(state State) { 
+	self.current = state
+}
+
+
+type State interface { 
+	Handle(c Context)
+}
+
+type ConcreteStateA struct {}
+
+func (self ConcreteStateA) Handle(c Context) { 
+	c.Transitition(new(ConcreteStateA))
+}
+
+func (self Context) Request() { 
+	self.current.Handle(self)
+}
+
+
+func main() { 
+	ctx := &Context{current: new(ConcreteStateA)}
+
+
+	ctx.Request()
+	ctx.Request()
+}

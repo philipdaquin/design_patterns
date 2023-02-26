6a8dbc20ea3d9b31a1d42b66fd8c1dcfefa46328
philipdaquin <philipdaquin20@gmail.com>
2023-02-27 07:54:55 +0800

    flight booking service

---
+++ low-level-designs/flight_booking_service/src/main/java/com/example/flight_booking_service/FlightBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.flight_booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingServiceApplication.class, args);
+	}
+
+}
---
+++ low-level-designs/flight_booking_service/src/test/java/com/example/flight_booking_service/FlightBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flight_booking_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

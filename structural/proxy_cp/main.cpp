c226e990acfe19eae11ab4ddf29497dbaec495bd
philipdaquin <philipdaquin20@gmail.com>
2023-03-14 07:15:19 +0800

    proxy cpp

---
+++ structural/proxy_cp/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+    The Proxy design pattern is a structural desing pattern that provides a surrogate or placeholder for 
+    another object to control access to it
+
+    The primary intent of the proxy pattern is to create a representative object that can act as an intermediatry between 
+    client and the real subject object 
+    - The client interacts with the proxy object, which in turn delegates the requests to the real object
+
+    - This allows the proxy object to add extra functionality to the real object or to retrict access to it 
+
+
+    Types
+    - Remote Proxy - local representation of a remote object 
+
+    - Virtual Proxy - 
+
+    - Protection Proxy 
+
+*/
+
+
+int main(int, char**) {
+    std::cout << "Hello, world!\n";
+}

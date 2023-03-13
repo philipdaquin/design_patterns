09bfd6f644b2df4f95059e407b9cf17255b58ae4
philipdaquin <philipdaquin20@gmail.com>
2023-03-14 07:29:24 +0800

    proxy cpp

--- structural/proxy_cp/main.cpp
+++ structural/proxy_cp/main.cpp
@@ -16,11 +16,58 @@ using namespace std;
     Types
     - Remote Proxy - local representation of a remote object 
 
-    - Virtual Proxy - 
+    - Virtual Proxy - A placeholder for an expensive or resouce intensive object, which is loaded only when t is needed
 
-    - Protection Proxy 
-
+    - Protection Proxy - allow or deny requests based on certain criteria 
 */
+
+// Declare the common operations for both realSubject and proxy 
+class Subject { 
+    // Inline 
+    public:
+    virtual ~Subject() {}
+    virtual void request() const = 0;
+};
+// The main object 
+class RealSubject: public Subject { 
+    public:
+    void request() const override { 
+        cout << "Real Subject: Handling Request" << endl;
+    }
+};
+
+// The proxy 
+class Proxy: public Subject { 
+    RealSubject* subject;
+    bool isAuthorised;
+    
+    public:
+    //  Construct 
+    Proxy(RealSubject& realSubject) : subject(new RealSubject(realSubject)), isAuthorised(false) {}
+    
+    ~Proxy() { 
+        delete subject;
+    }
+    // Maintain reference 
+    void request() const override { 
+        if (!isAuthorised) cout << "Proxy: Unauthorised request. Access denied." << endl;
+        else { 
+            subject -> request();
+        }
+    }
+
+    void authorised() { 
+        isAuthorised = true;
+    }
+
+
+
+};
+
+// Client code 
+void client_code(const Subject& subject) { 
+    subject.request();
+}
 
 
 int main(int, char**) {

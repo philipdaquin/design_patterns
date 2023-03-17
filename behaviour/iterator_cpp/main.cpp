c834a49e2cebbf71e7670c9146660beb5926bb20
philipdaquin <philipdaquin20@gmail.com>
2023-03-17 09:30:33 +0800

    cpp

--- behaviour/iterator_cpp/main.cpp
+++ behaviour/iterator_cpp/main.cpp
@@ -1,4 +1,44 @@
 #include <iostream>
+#include <vector>
+using namespace std;
+
+// Iterator interface
+template<typename T> class Iterator { 
+    
+    public:
+        virtual T next() = 0;
+        virtual bool hasNext() = 0;
+};
+
+// Concrete iterator implementation for a vector 
+template<typename T> class VectorIterator : public Iterator<T> { 
+    private:
+        int data;
+        size_t index;
+    public: 
+        VectorIterator(vector<T>& data_) : data(data_), index(0) {} 
+
+        T next() override { 
+            return data[index++];
+        }
+
+        bool hasNext() override { 
+            return index < data.size();
+        }
+};
+
+template<typename T> class Aggregate { 
+    public: 
+        virtual Iterator<T>* createIterator() = 0;
+};
+
+template<typename T> class VectorAggregate: public Aggregate<T> {
+    private:
+        vector<T> data:
+    Iterator<T>* createIterator() override { 
+        return new VectorIterator<T>(data);
+    }
+};
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";

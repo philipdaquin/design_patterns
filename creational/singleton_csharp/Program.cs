99ac1b344a44b1845eb7207e970b9adb6fe19fd5
philipdaquin <philipdaquin20@gmail.com>
2023-05-16 06:24:35 +0800

    singleton c sharp

---
+++ creational/singleton_csharp/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+namespace Singleton{
+    public class Singleton { 
+        private static Singleton instance;
+
+        private Singleton() {}
+
+        private static readonly object _lock = new object();
+
+        public static Singleton GetIntance(string value) { 
+            lock(_lock) {
+                if (instance == null) { 
+                    instance = new Singleton();
+                }
+            }
+            return instance;
+        }
+
+        public string Value { get; set; }
+    }
+
+    class Program { 
+        static void Main(string[] args) { 
+            
+            Singleton singleton = Singleton.GetIntance("Test");
+
+            Console.WriteLine(singleton.Value);
+        }
+    }
+}

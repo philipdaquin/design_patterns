d6546b59bef5e21edf396233ad551d9631106e1f
philipdaquin <philipdaquin20@gmail.com>
2023-05-27 08:47:10 +0800

    facade

--- structural/facade_csharp/Program.cs
+++ structural/facade_csharp/Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class SubSystemA { 
+    public void operation() { 
+        Console.WriteLine("Hello, World!");
+    }
+}
+class SubSystemB { 
+    public void operation() { 
+        Console.WriteLine("Hello, World!");
+    }
+}
+class SubSystemC { 
+    public void operation() { 
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+class Facade { 
+    private SubSystemA systemA;
+    private SubSystemB systemB;
+    private SubSystemC systemC;
+
+
+    public Facade() { 
+        systemA = new SubSystemA();
+        systemB = new SubSystemB();
+        systemC = new SubSystemC();
+    }
+
+    public void Operation() { 
+        Console.WriteLine("Facade Operation");
+        systemA.operation();
+        systemB.operation();
+        systemC.operation();
+    }
+}
+
+class Client { 
+    static void Main(string[] args) { 
+        Facade facade = new Facade();
+        facade.Operation();
+    } 
+}
+
+// See https://aka.ms/new-console-template for more information
+
+

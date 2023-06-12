6c394890c8910caffb027b5fcb0782ea8c19502b
philipdaquin <philipdaquin20@gmail.com>
2023-06-13 05:51:52 +0800

    factory

--- behaviour/factory_kot/src/main/kotlin/Main.kt
+++ behaviour/factory_kot/src/main/kotlin/Main.kt
@@ -1,7 +1,40 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+interface Shape {
+    fun draw()
+
+}
+
+class Circle: Shape {
+    override fun draw() {
+        println("Drawing a circle")
+    }
+}
+
+class Square: Shape {
+    override fun draw() {
+        println("Drawing a square")
+    }
+}
+class ShapeFactory {
+    fun createShape(shape: String): Shape? {
+        return when(shape.lowercase()) {
+            "circle" -> Circle()
+            "square" -> Square()
+            else -> null
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    val factory = ShapeFactory()
+
+    val circle = factory.createShape("circle")
+    circle?.draw()
+
+    val square = factory.createShape("square")
+    square?.draw()
+
+
+
 }

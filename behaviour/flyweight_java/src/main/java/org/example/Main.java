43e501f693c410b8ce46c517b137cd2c0a2eed65
philipdaquin <philipdaquin20@gmail.com>
2023-06-14 09:18:52 +0800

    flyweight java

---
+++ behaviour/flyweight_java/src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/*
+*   Flyweight design pattern
+*   - Aims to minimise the memory usage and improve performance by sharing as much
+*   data as possible among multiple objects.
+*   - It is used when we need to create a large number of similar objects that consume a significant amount of memory
+* */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

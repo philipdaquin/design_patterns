b707d3039f56becba89d691bcac0505991048b5d
philipdaquin <philipdaquin20@gmail.com>
2023-07-05 22:04:36 +0800

    abstract factory java

---
+++ creational/abstract_java/src/main/java/org/example/MacOSButton.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class MacOSButton implements Button {
+    @Override
+    public void render() {
+        System.out.println("Rendering a button in MacOS Style.");
+    }
+}
---
+++ creational/abstract_java/src/main/java/org/example/WindowsButton.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class WindowsButton implements Button {
+
+    @Override
+    public void render() {
+        System.out.println("Rendering a button in Windows Style");
+    }
+}

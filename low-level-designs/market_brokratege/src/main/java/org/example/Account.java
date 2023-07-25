2fe8f88205ff234406d24d3c1654c3966be41555
philipdaquin <philipdaquin20@gmail.com>
2023-07-25 18:18:05 +0800

    im not sure what this is but yolo

--- low-level-designs/market_brokratege/src/main/java/org/example/Account.java
+++ low-level-designs/market_brokratege/src/main/java/org/example/Account.java
@@ -1,2 +1,11 @@
-package org.example;public class Account {
+package org.example;
+
+public class Account {
+    private Integer id;
+    private String password;
+    private String name;
+    private Address address;
+    public AccountStatus status;
+    public String email;
+    private String phone;
 }

704b76066a172259f38ffb2a0a4f14e3c4c59d7d
philipdaquin <philipdaquin20@gmail.com>
2023-08-01 19:24:32 +0800

    c sharp abstract

---
+++ creational/abstract_csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿
+/*
+    Abstract Factory Pattern 
+    - An interface for creating families of related or dependent objects without 
+    specifying their concrete classes
+
+    - It allows you to create objects that follow a certain interface or theme while hiding the 
+    implementation detials of how those objects are created 
+
+
+*/
+namespace Abstract { 
+    class Program { 
+        static void Main() { 
+            Console.WriteLine("Hello word ");
+        }
+    }
+}
